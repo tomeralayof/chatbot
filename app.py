d6171e7b5af771d09a4cb967765e206e4a1ad55f
tomeralayof <tomeralayof95959@gmail.com>
2023-03-13 22:32:54 +0200

    update post url

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #app.py
 from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods = ["GET"])
-def index():
-    return "tomer alayof"
+@app.route("/send-message", methods = ["POST"])
+def userMessage():
+    message = request.json.get("message")
+    return jsonify(message)
