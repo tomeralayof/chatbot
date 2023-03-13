161aa2bd35339ee9e796a63ee410070415a01b40
tomeralayof <tomeralayof95959@gmail.com>
2023-03-13 16:25:53 +0200

    update procfile

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,5 +7,4 @@ def home():
     return "flask heruko app"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
