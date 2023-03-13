1dbb265c37e80571689076321b5a6dc8f6e94628
tomeralayof <tomeralayof95959@gmail.com>
2023-03-13 22:16:24 +0200

    add cors origion

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #app.py
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route("/", methods = ["GET"])
 def index():
-    return "tomer"
+    return "tomer alayof"
